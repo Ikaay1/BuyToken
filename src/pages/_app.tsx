ecebd5e2d722b991ca5f6f1bf9a5757e815c4cb7
Mgbemele Ikechukwu <mgbemeleikechukwu@gmail.com>
2023-09-15 22:52:36 +0100

    nprogress

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,29 @@
 import '@/styles/globals.css';
 
 import {AppProps} from 'next/app';
+import {Router} from 'next/router';
+import {useEffect} from 'react';
 
 import {theme} from '@/constants/theme';
 import Layout from '@/layouts/Layout';
 import {ChakraProvider} from '@chakra-ui/react';
 
+const NProgress = require('nprogress');
+
 export default function App({Component, pageProps}: AppProps) {
+  useEffect(() => {
+    NProgress.configure({showSpinner: false});
+    Router.events.on('routeChangeStart', (url: any) => {
+      NProgress.start();
+    });
+    Router.events.on('routeChangeComplete', (url: any) => {
+      NProgress.done();
+    });
+
+    Router.events.on('routeChangeError', (url: any) => {
+      NProgress.done();
+    });
+  }, [Router]);
   return (
     <ChakraProvider theme={theme}>
       <Layout>
