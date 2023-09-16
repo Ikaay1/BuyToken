794c49673dac3b8c6358caea5218fff17b53b909
Mgbemele Ikechukwu <mgbemeleikechukwu@gmail.com>
2023-09-16 20:12:09 +0100

    theme

---
+++ src/components/home/Navbar.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import { Box, Flex } from '@chakra-ui/react';
+
+const Navbar = () => {
+  return (
+    <Box px='4.5rem' pt='2rem' pb='10rem' bg='#4CAD73'>
+      <Flex></Flex>
+    </Box>
+  );
+};
+
+export default Navbar;
